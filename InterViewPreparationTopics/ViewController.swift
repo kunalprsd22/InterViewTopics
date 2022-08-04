50a41461f928849329a5aea5551b004091b1bc36
Kunal_iOS <kunal.prasad@appinventiv.com>
2022-08-04 14:42:33 +0530

    Initial Commit

---
+++ InterViewPreparationTopics/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InterViewPreparationTopics
+//
+//  Created by Appinventiv on 04/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
